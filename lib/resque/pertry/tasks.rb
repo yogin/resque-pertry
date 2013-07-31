2c9893b0b33f22b97cfc1bd4c924f4ac8d1d5f69
Anthony Powles <github@idreamz.net>
2013-07-31 14:12:08 -0700

    updated purger task to write pid to using PIDFILE environment, and set various config from PURGER_SLEEP, PURGER_LIMIT and VERBOSE

--- lib/resque/pertry/tasks.rb
+++ lib/resque/pertry/tasks.rb
@@ -7,6 +7,11 @@ namespace :resque do
   namespace :pertry do
     desc "Start Resque Pertry database and failed queue purger"
     task :purger => :pertry_setup do
+      File.open(ENV['PIDFILE'], 'w') { |f| f << Process.pid.to_s } if ENV['PIDFILE']
+
+      Resque::Pertry::Purger.sleep_time = Integer(ENV['PURGER_SLEEP']) if ENV['PURGER_SLEEP']
+      Resque::Pertry::Purger.failed_jobs_limit = Integer(ENV['PURGER_LIMIT']) if ENV['PURGER_LIMIT']
+      Resque::Pertry::Purger.verbose = true if ENV['VERBOSE']
       Resque::Pertry::Purger.run
     end
 
